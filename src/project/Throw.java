ebd41d996ba1b47b0c2fe85a8b1d468c4eaca122
Alex Diop <83488629+alexdiop@users.noreply.github.com>
2022-01-24 04:14:11 -0600

    Update Throw.java

--- src/project/Throw.java
+++ src/project/Throw.java
@@ -4,7 +4,7 @@ package project;
 // Author      : Alexander Diop
 // Created     : 12/6/21
 // Version     : 0.0.1
-// Project     : Bright Health Bowling
+// Project     : Bowling App
 // Description : Class object for a given bowling attempt by a player
 //
 //
